0e6e370c471532cef72a7f8040101c736565ca3a
ADDOUCHE Walid <73178026+walidaddouche@users.noreply.github.com>
2022-10-23 10:22:39 +0200

    Rectangle moving

--- src/main/java/engines/graphics/Engine.java
+++ src/main/java/engines/graphics/Engine.java
@@ -1,22 +1,27 @@
 package engines.graphics;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.Serial;
 import java.text.MessageFormat;
 
 import javax.swing.JPanel;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
  * An extension of javax.swing.JFrame that can draw images.
  */
-public class Engine extends JPanel {
+public class Engine extends JPanel implements KeyListener {
     @Serial
     private static final long serialVersionUID = 4242L;
     private transient BufferedImage image;
+    Rectangle rectangle = new Rectangle(400,100,100,100);
+
 
     /**
      * Constructs a new panel that draw an image.
@@ -32,6 +37,7 @@ public class Engine extends JPanel {
         }
         try {
             setImage(0,path);
+            this.addKeyListener(this);
         } catch (Exception ex) {
             String message = MessageFormat.format("Error: Cannot load image at path: {0}", path);
             logger.error(message, ex);
@@ -41,7 +47,10 @@ public class Engine extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(image, 50, 50, null);
+       // g.drawImage(image, 50, 50, null);
+        g.drawRect(rectangle.x, rectangle.y, rectangle.width,rectangle.height);
+
+
 
     }
 
@@ -50,7 +59,57 @@ public class Engine extends JPanel {
         ObjectsManager objectsManager = new ObjectsManager();
         objectsManager.setObjectEImage(i,path);
         image = objectsManager.getGraphicalObjects(i);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            System.out.println("Right key typed");
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            System.out.println("Left key typed");
+        }
+
+    }
+    @Override
+    public void setFocusable(boolean b) {
+        super.setFocusable(b);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            rectangle.x = rectangle.x + 10 ;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            rectangle.x = rectangle.x - 10 ;
+
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            rectangle.y = rectangle.y + 10 ;
+
+        }
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+            rectangle.y = rectangle.y - 10 ;
+
+        }
+
+        repaint();
 
     }
 
-}
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            System.out.println("Right key Released");
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            System.out.println("Left key Released");
+        }
+
+    }
+
+
+    }
--- src/main/java/sample/MyJavaFrame.java
+++ src/main/java/sample/MyJavaFrame.java
@@ -3,6 +3,8 @@ package sample;
 import engines.graphics.Engine;
 import engines.physics.Direction;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.io.Serial;
 import javax.swing.JFrame;
@@ -11,24 +13,52 @@ import javax.swing.JFrame;
 /**
  * An extended version of javax.swing.JFrame containing a panel to draw images.
  */
-public class MyJavaFrame extends JFrame {
+public class MyJavaFrame extends JFrame implements KeyListener {
 
-  @Serial
-  private static final long serialVersionUID = 42L;
-  /**
-   * Constructs a new visible frame.
-   */
+    @Serial
+    private static final long serialVersionUID = 42L;
 
-  public MyJavaFrame() throws IOException {
-    setTitle("Main window");
-    setSize(800, 800);
-    setLocationRelativeTo(null);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    add(new Engine());
-    setVisible(true);
-  }
+    Engine engine = new Engine();
 
-  public static void main(String[] args) throws IOException {
-    new MyJavaFrame();
-  }
+
+    /**
+     * Constructs a new visible frame.
+     */
+
+    public MyJavaFrame() throws IOException {
+        this.addKeyListener(this);
+        setTitle("Main window");
+        setSize(800, 800);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(engine);
+        engine.setFocusable(true);
+        setVisible(true);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        new MyJavaFrame();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+       engine.keyTyped(e);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        engine.keyPressed(e);
+
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        engine.keyReleased(e);
+
+
+    }
 }
